9063730d21e2e240b95d1531907e5158707e084d
sreemanrepaka <sreemanrepaka@gmail.com>
2023-04-01 23:43:59 +0530

    improved extension ui

--- fetch.js
+++ fetch.js
@@ -7,19 +7,37 @@
 // console.log(selectedText);
 document.getElementById("btn").addEventListener("click", function() {
   let tweet=document.getElementById('text').value;
+  if (tweet.length > 0) {
     axios.post('http://127.0.0.1:5000/predict', {
     text: tweet
     
   })
     .then(
         (response)=>{
-           document.getElementById('result').innerHTML = response.data;
+          if (response.data!=="not bullying"){
+            document.getElementById('result').innerHTML ="Cyberbullying detected! Type of cyberbullying: " + response.data;
+            document.getElementById('result').style.backgroundColor="red";
+            alert("Cyberbullying detected! Report the tweet immediately!")
+
+          }
+
+          else{
+            document.getElementById('result').innerHTML = "No cyberbullying detected!";
+            document.getElementById('result').style.backgroundColor="green";
+          }
         }
+
         )
     .catch(error => console.error(error));
 
+    }
+
+    else{
+      document.getElementById('result').innerHTML = "No text entered";
+    }
     
 
 });
+
 
 
