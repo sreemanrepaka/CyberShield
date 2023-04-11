a74aaa9a5ded783ffa04992d011add51818a22df
sreemanrepaka <sreemanrepaka@gmail.com>
2023-04-11 22:34:23 +0530

    added tweet scraping functionality

--- api.py
+++ api.py
@@ -30,9 +30,67 @@ def predict():
 
     # Return the prediction as a JSON response
     # prediction=jsonify({'prediction': prediction})
-    sentiments = ["age", "not bullying", "racism", "religion", "gender"]
+    sentiments = ["age", "not bullying", "miscellaneous","racism", "religion", "gender"]
     all_categories_names = np.array(sentiments)
     return all_categories_names[prediction]
+
+@app.route('/predict', methods=['GET'])
+def web_scrape():
+    # "C:\Users\sreem\Downloads\chromedriver_win32\chromedriver.exe"
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+
+    # Set up Chrome options
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+
+    # Set up ChromeDriver executable path
+    chromedriver_path = r'C:\Users\sreem\Downloads\chromedriver_win32\chromedriver.exe'
+    service = Service(executable_path=chromedriver_path)
+    # Create a new ChromeDriver instance with the service and options
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    driver.get('http://localhost:8080')
+
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.common.exceptions import TimeoutException
+    import requests
+
+    # Wait for the div to be loaded
+    try:
+        # Wait for the div element to be present
+        divs = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.q-list.q-pa-sm'))
+        )
+
+        # Print the text contents of the div element
+        lst = []
+        res=[]
+        for div in divs:
+            tweets = div.text.split('\n')
+            lst.append(tweets[1])
+        for i in lst:
+            url = 'http://127.0.0.1:5000/predict'
+            myobj = {'text': i}
+
+            x = requests.post(url, json=myobj)
+            res.append(x.text)
+        return res
+
+
+
+
+    except TimeoutException:
+        print("Timed out waiting for the div element to be present")
+
+    finally:
+        # Quit the driver
+        driver.quit()
+
+
+
 
 # Run the app
 if __name__ == '__main__':
--- fetch.js
+++ fetch.js
@@ -35,3 +35,56 @@ document.getElementById("btn").addEventListener("click", function() {
 
 
 
+
+document.getElementById("btn2").addEventListener("click", function() {
+
+  let bully=0;
+  let non_bully=0;
+  let total=0;
+
+
+axios.get('http://127.0.0.1:5000/predict')
+  .then(function (response) {
+    if (response.data.length > 0) {
+      for (let index = 0; index < response.data.length; index++) {
+        total++;
+        if (response.data[index]=="not bullying"){
+          non_bully++;
+
+
+        }
+        else{
+          bully++;
+        }
+ 
+      }
+      document.getElementById('result').innerHTML =""
+      
+      document.getElementById('stats-container').style.visibility="visible";
+      document.getElementById('total').innerHTML =total;
+      document.getElementById('bullying').innerHTML =bully;
+      document.getElementById('not-bullying').innerHTML =non_bully;
+      alert("Cyberbullying detected in "+bully+" tweet(s). Report the tweets immediately!")
+
+      
+    }
+
+   
+  })
+  .catch(error => console.log(error))
+
+
+  setTimeout(() => {
+    document.getElementById('result').innerHTML = "Scraping tweets...";
+  }, 1000);
+
+  setTimeout(() => {
+    document.getElementById('result').innerHTML = "Calling API...";
+  }, 4000);
+
+  setTimeout(() => {
+    document.getElementById('result').innerHTML = "Fetching results...";
+  }, 7000);
+
+  
+});
